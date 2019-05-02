b9f583fb7dc17ed446b4f4f2177bef2e83fdb40e
Tatiana Egorova <margobts@gmail.com>
2019-05-02 20:30:48 +0300

    add aip service manager, 1st iteration

---
+++ Assets/Scripts/Managers/IAPManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Purchasing;
+
+public class IAPManager : MonoBehaviour, IStoreListener {
+    private void Start() {
+       // var builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
+       // builder.AddProduct();
+    }
+
+    public void OnInitializeFailed(InitializationFailureReason error) {
+        throw new System.NotImplementedException();
+    }
+
+    public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs e) {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPurchaseFailed(Product i, PurchaseFailureReason p) {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnInitialized(IStoreController controller, IExtensionProvider extensions) {
+        throw new System.NotImplementedException();
+    }
+}
